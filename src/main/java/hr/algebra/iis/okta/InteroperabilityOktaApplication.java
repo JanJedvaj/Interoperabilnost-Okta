ea1822709918ce108286b184c5c674ffba7e7c79
JanJedvaj <jan.jedvaj2@gmail.com>
2026-06-24 22:10:46 +0200

    initial folder structure

---
+++ src/main/java/hr/algebra/iis/okta/InteroperabilityOktaApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.iis.okta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteroperabilityOktaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteroperabilityOktaApplication.class, args);
+    }
+}
+
